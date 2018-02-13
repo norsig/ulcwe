9208ab0ef73e408fc7775845f7b1fd11ee7b6267
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-13 19:20:17 +0200

    Update install_primary.sh

--- install_primary.sh
+++ install_primary.sh
@@ -11,5 +11,8 @@ printf "%s\n" "source $HOME/$repo/configurations/variables.sh" >> $HOME/.bashrc
 printf "%s\n" "source $HOME/$repo/configurations/aliases.sh" >> $HOME/.bashrc
 source "$HOME"/.bashrc 2>/dev/null
 
-bash "$HOME"/"$repo"/install_secondary.sh
+"$HOME"/"$repo"/external_software/{basics.sh,pma.sh,wp-cli.sh}
+"$HOME"/"$repo"/configurations/{nginx.sh,php.sh}
+mv "$HOME"/"$repo"/templates/cron_daily /etc/cron.daily/cron_daily,cron_daily}
+
 "$rse"
