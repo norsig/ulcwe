37db0ee199e30a843959bdc35fa13c6d8f57c4a9
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-13 18:20:18 +0200

    Update install_primary.sh

--- install_primary.sh
+++ install_primary.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
+export repo="ulcwe"
 
-downloadProject() {
-	export repo="ulcwe"
-	apt-get update -y
-	apt-get upgrade git -y
-	git clone https://github.com/benqzq/"$repo" --depth 1 --branch=master "$HOME"/"$repo"/
-	rm -rf "$HOME"/"$repo"/.git/
-	chmod +x "$HOME"/"$repo"/* -R
-}
-downloadProject
+apt-get update -y
+apt-get upgrade git -y
+git clone https://github.com/benqzq/"$repo" --depth 1 --branch=master "$HOME"/"$repo"/
+rm -rf "$HOME"/"$repo"/.git/
+chmod +x "$HOME"/"$repo"/* -R
 
-utilizeProject() {
-	printf "%s\n" "source $HOME/$repo/configurations/variables.sh" >> $HOME/.bashrc
-	printf "%s\n" "source $HOME/$repo/configurations/aliases.sh" >> $HOME/.bashrc
-	source "$HOME"/.bashrc 2>/dev/null
-	bash "$HOME"/"$repo"/install/install_secondary.sh
-	"$rse"
-}
-utilizeProject
+printf "%s\n" "source $HOME/$repo/configurations/variables.sh" >> $HOME/.bashrc
+printf "%s\n" "source $HOME/$repo/configurations/aliases.sh" >> $HOME/.bashrc
+source "$HOME"/.bashrc 2>/dev/null
+
+bash "$HOME"/"$repo"/install_secondary.sh
+"$rse"
