a631dadfd780dc093d60e0c0ad778f186ae4548b
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-13 20:53:36 +0200

    Update php.sh

--- configuration/php.sh
+++ configuration/php.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-phpConf() {
-	for dir in /etc/php/*/fpm/; do
-		cat <<-EOF > "$dir"/zz_overrides.ini
-			[PHP]
-			post_max_size = 2000M
-			upload_max_filesize = 2000M
-			max_execution_time = 3000
-			cgi.fix_pathinfo=0
-		EOF
-	done
-}
-phpConf
+for dir in /etc/php/*/fpm/; do
+cat <<-EOF > "$dir"/zz_overrides.ini
+	[PHP]
+	post_max_size = 2000M
+	upload_max_filesize = 2000M
+	max_execution_time = 3000
+	cgi.fix_pathinfo=0
+EOF
+done
 "$rse"
