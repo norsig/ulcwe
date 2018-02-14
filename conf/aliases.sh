471eb8250df019da5b0e7e9fbc1bc3580045aae7
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 08:16:11 +0200

    Update aliases.sh

--- conf/aliases.sh
+++ conf/aliases.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 alias drt="cd $drt/"
 alias rse="$rse"
+
+alias tdm="tmux new-session -d 'bash $HOME/$repo/internal_software/tdm.sh'"
 alias nxd="$HOME/$repo/internal_software/nxd.sh"
 alias imb="$HOME/$repo/internal_software/imb.sh"
-alias tdm="tmux new-session -d 'bash $HOME/$repo/internal_software/tdm.sh'"
 alias nwsm="$HOME/$repo/internal_software/nwsm.sh"
 alias nwsm_undo="$HOME/$repo/internal_software/nwsm_undo.sh"
