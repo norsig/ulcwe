b460ddbb1cdb70597dc58b32e063e76100b5c08f
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 11:54:25 +0200

    Update secondary.sh

--- external_software/secondary.sh
+++ external_software/secondary.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-phpmyadmin() {
+# PHPMYADMIN:
 	cd "$drt"
-	rm -rf ${pma}* # Regex-Wildcard Combo (RWC): No variable expansion quoting allowed; 
+	rm -rf ${pma}* # Regex-Wildcard Combo (RWC): Don't double-quote variable expansions; 
 	wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
 	unzip ${pma}*.zip # RWC;
 	mv ${pma}*/ phpmyadmin/ # RWC;
 	rm ${pma}*.zip # RWC;
 	chmod -R 000 "$drt"/phpmyadmin/
 	cd
-}
-phpmyadmin
 
-wp-cli() {
+# WP-CLI:
 	curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
 	chmod +x /usr/local/bin/wp
-}
-wp-cli
