5651ccbd6ce66c06effac2a9a32e7f4a845ac110
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 08:20:08 +0200

    Update nwsm_undo.sh

--- internal_software/nwsm_undo.sh
+++ internal_software/nwsm_undo.sh
@@ -3,7 +3,6 @@ domain="$1" && test -z "$domain" && exit 2
 read -sp "DB user password:" dbrootp
 
 rm -rf "$drt"/"$domain"/
-
 rm "$s_a"/"$domain".conf "$s_e"/"$domain".conf
 
 cat <<-DBSTACK | mysql -u root -p"$dbrootp"
@@ -11,5 +10,4 @@ cat <<-DBSTACK | mysql -u root -p"$dbrootp"
 	DROP database IF EXISTS "$domain";
 DBSTACK
 
-rm -rf /etc/letsencrypt/{live,renewal,archive}/"$DOMAIN","$DOMAIN".conf}
 "$rse"
