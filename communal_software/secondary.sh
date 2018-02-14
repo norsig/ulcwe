873345fec99903a13483fa49d70f0c85e06a7670
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 12:01:54 +0200

    Update secondary.sh

--- communal_software/secondary.sh
+++ communal_software/secondary.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# PHPMYADMIN:
-	cd "$drt"
-	rm -rf ${pma}* # Regex-Wildcard Combo (RWC): Don't double-quote variable expansions; 
-	wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
-	unzip ${pma}*.zip # RWC;
-	mv ${pma}*/ phpmyadmin/ # RWC;
-	rm ${pma}*.zip # RWC;
-	chmod -R 000 "$drt"/phpmyadmin/
-	cd
+cd "$drt"
+rm -rf ${pma}* # Regex-Wildcard Combo (RWC): Don't double-quote variable expansions; 
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
+unzip ${pma}*.zip # RWC;
+mv ${pma}*/ phpmyadmin/ # RWC;
+rm ${pma}*.zip # RWC;
+chmod -R 000 "$drt"/phpmyadmin/
+cd
 
-# WP-CLI:
-	curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
-	chmod +x /usr/local/bin/wp
+curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
+chmod +x /usr/local/bin/wp
