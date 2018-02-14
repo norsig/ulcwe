ec8ffaf23718adf8f127e07dde1b956fc5519497
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 08:43:04 +0200

    Update pma.sh

--- external_software/pma.sh
+++ external_software/pma.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-cd "$drt"
-rm -rf "$pma"*
-wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
-unzip "$pma"*.zip
-mv "$pma"*/ phpmyadmin/
-rm "$pma"*.zip
-chmod -R 000 "$drt"/phpmyadmin/
-cd
+
+phpmyadmin() {
+	cd "$drt"
+	rm -rf "$pma"*
+	wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
+	unzip "$pma"*.zip
+	mv "$pma"*/ phpmyadmin/
+	rm "$pma"*.zip
+	chmod -R 000 "$drt"/phpmyadmin/
+	cd
+}
+phpmyadmin
+
+wp-cli() {
+	curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
+	chmod +x /usr/local/bin/wp
+}
+wp-cli
