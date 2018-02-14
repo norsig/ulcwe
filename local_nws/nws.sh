43274b73e047f9a5356b1a7fcdd34c506dcc67d4
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 14:53:31 +0200

    Update nws.sh

--- local_nws/nws.sh
+++ local_nws/nws.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-cat <<-EOF > "$s_a/$domain.conf"
-server {
-	root ${drt}/${domain}/;
-	server_name ${domain} www.${domain};
-	location ~* \.(jpg|jpeg|png|gif|ico|css|js|ttf|woff|pdf)$ {expires 365d;}
-	location "/wp-login\.php" {access_log "/var/log/httpd/wordpress-logins.log";}
-}
-EOF
-ln -sf "$s_a"/"$domain".conf "$s_e"
-
-cat <<-EOF | mysql -u root -p"$dbrootp"
-	CREATE USER "$domain"@"localhost" IDENTIFIED BY "$dbuserp";
-	CREATE DATABASE "$domain";
-	GRANT ALL PRIVILEGES ON "$domain".* TO "$domain"@"localhost";
-EOF
-
 "$rse"
 certbot --nginx -d "$domain" -d www."$domain"
 "$rse"
