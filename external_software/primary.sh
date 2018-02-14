d5bba1787c0b396e9a2fb9a68a1f84cf9bb3cea0
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 11:34:44 +0200

    Update primary.sh

--- external_software/primary.sh
+++ external_software/primary.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-ufw --force enable
-ufw allow 22,25,80,443,9000/tcp
-apt-get upgrade unattended-upgrades sshguard -y
 
-apt-get upgrade nginx mysql-server php-fpm php-mysql -y
-apt-get upgrade php-mbstring php-mcrypt -y
+apt() {
+	apt-get upgrade unattended-upgrades sshguard -y
 
-add-apt-repository ppa:certbot/certbot -y
-apt-get update -y
-apt-get upgrade python-certbot-nginx -y
+	apt-get upgrade nginx mysql-server php-fpm php-mysql -y
+	apt-get upgrade php-mbstring php-mcrypt -y
 
-apt-get upgrade zip unzip tree -y
-DEBIAN_FRONTEND=noninteractive apt-get upgrade postfix -y
+	add-apt-repository ppa:certbot/certbot -y
+	apt-get update -y
+	apt-get upgrade python-certbot-nginx -y
+
+	apt-get upgrade zip unzip tree -y
+	DEBIAN_FRONTEND=noninteractive apt-get upgrade postfix -y
+}
+apt
