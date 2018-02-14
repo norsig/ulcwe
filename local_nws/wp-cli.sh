918f39610e349a3fd2f2fecdad772f6710c765c3
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 14:50:53 +0200

    Create wp-cli.sh

---
+++ local_nws/wp-cli.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wp core download --path="$drt"/"$domain"/ --allow-root
+wp config create --path="$drt"/"$domain"/ --dbname="$domain" --dbuser="$domain" --dbpass="$dbuserp" --dbhost="localhost" --allow-root
