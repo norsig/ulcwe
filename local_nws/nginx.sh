8b8b96fab6038653e9412d9a29ca3d953f501851
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-15 01:16:15 +0200

    Update nginx.sh

--- local_nws/nginx.sh
+++ local_nws/nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cat <<-EOF > "$s_a/$domain.conf"
-server {
-	root ${drt}/${domain}/;
-	server_name ${domain} www.${domain};
-	location ~* \.(jpg|jpeg|png|gif|ico|css|js|ttf|woff|pdf)$ {expires 365d;}
-	location "/wp-login\.php" {access_log "/var/log/httpd/wordpress-logins.log";}
-}
+	server {
+		root ${drt}/${domain}/;
+		server_name ${domain} www.${domain};
+		location ~* \.(jpg|jpeg|png|gif|ico|css|js|ttf|woff|pdf)$ {expires 365d;}
+		location "/wp-login\.php" {access_log "/var/log/httpd/wordpress-logins.log";}
+	}
 EOF
 ln -sf "$s_a"/"$domain".conf "$s_e"
