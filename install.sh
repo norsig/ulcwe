0170b27d701db3763a63a37f3082a9b8a89e2fa4
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 13:47:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-export repo="ulcwe"
-apt-get update -y
-apt-get upgrade git -y
-git clone https://github.com/benqzq/"$repo" --depth 1 --branch=master "$HOME"/"$repo"/
-rm -rf "$HOME"/"$repo"/.git/
-chmod +x "$HOME"/"$repo"/* -R
 
-printf "%s\n" "source $HOME/$repo/conf/assignments.sh" >> $HOME/.bashrc
-source "$HOME"/.bashrc 2>/dev/null
+function localize() {
+	export repo="ulcwe"
+	apt-get update -y
+	apt-get upgrade git -y
+	git clone https://github.com/benqzq/"$repo" --depth 1 --branch=master "$HOME"/"$repo"/
+	rm -rf "$HOME"/"$repo"/.git/
+	chmod +x "$HOME"/"$repo"/* -R
+	printf "%s\n" "source $HOME/$repo/conf/assignments.sh" >> $HOME/.bashrc
+	source "$HOME"/.bashrc 2>/dev/null
+}
+localize
 
-"$HOME"/"$repo"/communal_software/primary.sh
-"$HOME"/"$repo"/communal_software/secondary.sh
+function install() {
+	"$HOME"/"$repo"/communal_software/primary.sh
+	"$HOME"/"$repo"/communal_software/secondary.sh
 
-"$HOME"/"$repo"/conf/nginx_default.sh
-"$HOME"/"$repo"/conf/php.sh
-mv "$HOME"/"$repo"/conf/cron_daily /etc/cron.daily/cron_daily
-mv "$HOME"/"$repo"/conf/cron_weekly /etc/cron.daily/cron_weekly
+	"$HOME"/"$repo"/conf/nginx_default.sh
+	"$HOME"/"$repo"/conf/php.sh
 
-"$rse"
+	mv "$HOME"/"$repo"/conf/cron_daily /etc/cron.daily/cron_daily
+	mv "$HOME"/"$repo"/conf/cron_weekly /etc/cron.daily/cron_weekly
+
+	"$rse"
+}
+install
