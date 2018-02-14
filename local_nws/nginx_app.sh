23ab0045ca4597ddd17929ba008e69852d403f65
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 14:51:43 +0200

    Create nginx_app.sh

---
+++ local_nws/nginx_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat <<-EOF > "$s_a/$domain.conf"
+server {
+	root ${drt}/${domain}/;
+	server_name ${domain} www.${domain};
+	location ~* \.(jpg|jpeg|png|gif|ico|css|js|ttf|woff|pdf)$ {expires 365d;}
+	location "/wp-login\.php" {access_log "/var/log/httpd/wordpress-logins.log";}
+}
+EOF
+ln -sf "$s_a"/"$domain".conf "$s_e"
