b7b55645bc685e987dbc36d39970e643468e9b2c
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-15 01:13:53 +0200

    Update nws.sh

--- local_nws/nws.sh
+++ local_nws/nws.sh
@@ -9,7 +9,6 @@ read -sp "Please enter DB user password:      " dbuserp_1 \
 read -sp "Please enter DB user password again:" dbuserp_2 \
 if [ "$dbuserp_1" != "$dbuserp_2" ]; then echo "Values unmatched" && exit 1 fi
 
-"$PWD"/tests.sh
 "$PWD"/wp-cli.sh
 "$PWD"/nginx.sh
 "$PWD"/dbstack.sh
