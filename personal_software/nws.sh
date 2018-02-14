462e0f3ede87521532f1e162fcb5bb2b8cddad40
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-14 13:06:00 +0200

    Update nws.sh

--- personal_software/nws.sh
+++ personal_software/nws.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 domain="$1" && test -z "$domain" && exit 2
-read -sp "DB user password:" dbrootp
-read -sp "DB user password:" dbuserp
+
+read -sp "DB user password:" dbrootp_1       && echo
+read -sp "DB user password again:" dbuserp_2 && echo
+read -sp "DB user password:" dbrootp_1       && echo
+read -sp "DB user password again:" dbuserp_2 && echo
+
+if [ "$dbrootp_1" != "$dbrootp_2" ]; then echo "Values unmatched" && exit 1 fi
+if [ "$dbuserp_1" != "$dbuserp_2" ]; then echo "Values unmatched" && exit 1 fi
 
 wp core download --path="$drt"/"$domain"/ --allow-root
 wp config create --path="$drt"/"$domain"/ --dbname="$domain" --dbuser="$domain" --dbpass="$dbuserp" --dbhost="localhost" --allow-root
