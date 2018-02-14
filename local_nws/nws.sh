328628d419fa90458f4c4103972a1ae7e22509b1
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-15 01:20:27 +0200

    Update nws.sh

--- local_nws/nws.sh
+++ local_nws/nws.sh
@@ -3,11 +3,11 @@ domain="$1" && test -z "$domain" && exit 2
 
 read -sp "Please enter DB root password:      " dbrootp_1 \
 read -sp "Please enter DB root password again:" dbrootp_2 \
-if [ "$dbrootp_1" != "$dbrootp_2" ]; then echo "Values unmatched" && exit 1 fi
+if [ "$dbrootp_1" != "$dbrootp_2" ]; then echo "Values unmatched. Please try again." && exit 1 fi
 
 read -sp "Please enter DB user password:      " dbuserp_1 \
 read -sp "Please enter DB user password again:" dbuserp_2 \
-if [ "$dbuserp_1" != "$dbuserp_2" ]; then echo "Values unmatched" && exit 1 fi
+if [ "$dbuserp_1" != "$dbuserp_2" ]; then echo "Values unmatched. Please try again." && exit 1 fi
 
 "$PWD"/wp-cli.sh
 "$PWD"/nginx.sh
