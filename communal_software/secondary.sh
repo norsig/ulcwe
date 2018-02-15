1c484e876d8e2d86667929ce60b7c3f78a3a6d91
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-15 14:11:10 +0200

    Update secondary.sh

--- communal_software/secondary.sh
+++ communal_software/secondary.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-cd "$drt"
-rm -rf ${pma}* # Regex-Wildcard Combo (RWC): variable expansions aren't quoted; 
-wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
-unzip ${pma}*.zip # RWC;
-mv ${pma}*/ phpmyadmin/ # RWC;
-rm ${pma}*.zip # RWC;
-chmod -R 000 "$drt"/phpmyadmin/
-cd
 
-curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
-chmod +x /usr/local/bin/wp
+phpmyadmin() {
+	cd "$drt"
+	rm -rf ${pma}* # Regex-Wildcard Combo (RWC): variable expansions aren't quoted; 
+	wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
+	unzip ${pma}*.zip # RWC;
+	mv ${pma}*/ phpmyadmin/ # RWC;
+	rm ${pma}*.zip # RWC;
+	chmod -R 000 "$drt"/phpmyadmin/
+	cd
+}
+phpmyadmin
+
+wp-cli() {
+	curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
+	chmod +x /usr/local/bin/wp
+}
+wp-cli
