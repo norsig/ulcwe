5e2fa75d6db529d197ab3b51f12f334843540afe
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-15 19:01:57 +0200

    Update assignments.sh

--- conf/assignments.sh
+++ conf/assignments.sh
@@ -2,11 +2,11 @@
 
 envars_tertiary() {
 	set -a
-		s_a="/etc/nginx/sites-available"
-		s_e="/etc/nginx/sites-enabled"
-		drt="/var/www/html"
-		pma="[pP][hH][pP][mM][yY][aA][dD][mM][iI][nN]"
-		rse="misc/rse.sh"
+	s_a="/etc/nginx/sites-available"
+	s_e="/etc/nginx/sites-enabled"
+	drt="/var/www/html"
+	pma="[pP][hH][pP][mM][yY][aA][dD][mM][iI][nN]"
+	rse="misc/rse.sh"
 	set +a
 }
 envars_tertiary
