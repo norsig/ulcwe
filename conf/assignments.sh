56b17f31f1cfaa0b0130677626fc5104839e6ad0
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-15 14:13:08 +0200

    Update assignments.sh

--- conf/assignments.sh
+++ conf/assignments.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-set -a
-repo="ulcwe"
-s_a="/etc/nginx/sites-available"
-s_e="/etc/nginx/sites-enabled"
-drt="/var/www/html"
-pma="[pP][hH][pP][mM][yY][aA][dD][mM][iI][nN]"
-rse="$HOME/$repo/personal_software/rse.sh"
-set +a
 
-alias drt="cd $drt/"
-alias rse="$HOME/$repo/personal_software/rse.sh"
-alias nxd="$HOME/$repo/personal_software/nxd.sh"
-alias imb="$HOME/$repo/personal_software/imb.sh"
-alias tdm="tmux new-session -d 'bash $HOME/$repo/personal_software/tdm.sh'"
+variables() {
+	set -a
+		repo="ulcwe"
+		s_a="/etc/nginx/sites-available"
+		s_e="/etc/nginx/sites-enabled"
+		drt="/var/www/html"
+		pma="[pP][hH][pP][mM][yY][aA][dD][mM][iI][nN]"
+		rse="$HOME/$repo/personal_software/rse.sh"
+	set +a
+}
+variables
 
-alias nws="$HOME/$repo/personal_software/nws.sh"
+aliases() {
+	alias drt="cd $drt/"
+	alias rse="$HOME/$repo/personal_software/rse.sh"
+	alias nxd="$HOME/$repo/personal_software/nxd.sh"
+	alias imb="$HOME/$repo/personal_software/imb.sh"
+	alias tdm="tmux new-session -d 'bash $HOME/$repo/personal_software/tdm.sh'"
+
+	alias nws="$HOME/$repo/personal_software/nws.sh"
+}
+aliases
