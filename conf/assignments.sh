0d7b26b4e64a438107d05936f51606ec3de328b7
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-15 15:26:03 +0200

    Update assignments.sh

--- conf/assignments.sh
+++ conf/assignments.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-variables() {
+nonenvar_variables() {
 	set -a
-		repo="ulcwe"
 		s_a="/etc/nginx/sites-available"
 		s_e="/etc/nginx/sites-enabled"
 		drt="/var/www/html"
 		pma="[pP][hH][pP][mM][yY][aA][dD][mM][iI][nN]"
-		rse="$HOME/$repo/personal_software/rse.sh"
+		
+		rse="misc/rse.sh"
 	set +a
 }
-variables
+nonenvar_variables
 
 aliases() {
 	alias drt="cd $drt/"
-	alias rse="$HOME/$repo/personal_software/rse.sh"
-	alias nxd="$HOME/$repo/personal_software/nxd.sh"
+	alias rse="comm/rse.sh"
+	alias nxd="comm/nxd.sh"
 	alias imb="$HOME/$repo/personal_software/imb.sh"
 	alias tdm="tmux new-session -d 'bash $HOME/$repo/personal_software/tdm.sh'"
 
-	alias nws="$HOME/$repo/personal_software/nws.sh"
+	alias nws="$HOME/$repo/personal_software/nwsm.sh"
 }
 aliases
