8b1ec4ea19a51953984222aa8b17fcc6f55f14ff
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-16 12:21:48 +0200

    Update and rename cron_daily to cronjobs.sh

---
+++ conf/cronjobs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cat <<-EOF > /etc/cron.daily/cron_daily
+	#!/bin/bash
+	for dir in "$drt"/*/; do
+		if pushd "$dir"; then
+			wp plugin update --all --allow-root
+			wp core update --allow-root
+			wp language core update --allow-root
+			wp theme update --all --allow-root
+			"$rse"
+		popd
+		fi
+	done
+EOF
+
+cat <<-EOF > /etc/cron.weekly/cron_weekly
+	#!/bin/bash
+	find "${drt}" -path "*/cache/*" -type f -delete
+	certbot renew -q
+EOF
