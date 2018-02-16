c9ed960b4d450cfb5007c26e0f51e008cf1168f6
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-16 15:55:38 +0200

    Update primary.sh

--- primary.sh
+++ primary.sh
@@ -5,9 +5,9 @@ drt() {
 }
 
 nxd() {
-    printf "\n\n General: \n\n" && nginx -t
-    printf "\n\n Access: \n\n"  && tail /var/log/nginx/access.log
-    printf "\n\n Errors: \n\n"  && tail /var/log/nginx/error.log
+    printf "\n\n General: \n\n" && nginx -t                       && echo
+    printf "\n\n Access: \n\n"  && tail /var/log/nginx/access.log && echo
+    printf "\n\n Errors: \n\n"  && tail /var/log/nginx/error.log  && echo
 }
 
 imb() {
