6ed5ed464a1dd58647c8ef5571161e6fc19e7fa8
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-16 12:34:01 +0200

    Update wp-cli.sh

--- nwsm/wp-cli.sh
+++ nwsm/wp-cli.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 wp core download --path="$drt"/"$domain"/ --allow-root
 wp config create --path="$drt"/"$domain"/ --dbname="$domain" --dbuser="$domain" --dbpass="$dbuserp" --dbhost="localhost" --allow-root
-"$rse"
