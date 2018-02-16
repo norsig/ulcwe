5981c5a1605a2cb0051669cffac7175f46e0c7e9
benqzq <31392185+benqzq@users.noreply.github.com>
2018-02-16 16:32:02 +0200

    Update and rename conf_nginx.sh to conf_lemp.sh

--- conf_lemp.sh
+++ conf_lemp.sh
@@ -14,3 +14,14 @@ cat <<-EOF > "$s_a"/default
 		}
 	}
 EOF
+
+for dir in /etc/php/*/fpm/; do
+cat <<-EOF > "$dir"/zz_overrides.ini
+	[PHP]
+	post_max_size = 2000M
+	upload_max_filesize = 2000M
+	max_execution_time = 3000
+	cgi.fix_pathinfo=0
+EOF
+done
+"$rse"
